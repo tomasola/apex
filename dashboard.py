4594daa0ac80565af5f23178de0b3b548405d322
tomasola <tomas3050@gmail.com>
2026-03-06 05:33:03 +0100

    Render fix: switching from eventlet to gevent for better stability

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+import gevent.monkey
+gevent.monkey.patch_all()
 
 from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, emit
